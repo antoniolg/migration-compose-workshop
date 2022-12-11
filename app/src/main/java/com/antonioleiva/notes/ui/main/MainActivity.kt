350927839afad9e6532c8084cf70b566296717ce
antoniolg <antonio@devexperto.com>
2022-12-11 23:04:42 +0100

    Initial Commit

---
+++ app/src/main/java/com/antonioleiva/notes/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.antonioleiva.notes.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.antonioleiva.notes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
